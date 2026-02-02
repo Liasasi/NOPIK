2fdcfe4de344bee0cdd80b2bb6e947834c2405e1
Liasasi <liasasi1230@gmail.com>
2026-02-02 16:37:53 +0200

    fix port

--- NOPIK-GAME/server.js
+++ NOPIK-GAME/server.js
@@ -76,7 +76,7 @@ io.on("connection", (socket) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
-});
   console.log("NOPIK server running on 3000");
 
 });
+
